1cc0198341be6c6c2c60aa8d0904fa46eefa24ea
YEDAGNE Mel Ange Anicet <yedagneanicet@gmail.com>
2024-02-27 13:52:14 +0000

    Mon premier templates creer

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Band
+from .models import Listing
+
+
+def hello(request):
+    bands = Band.objects.all()
+    listing = Listing.objects.all()
+    return render(request, 'listings/hello.html',
+                  context={'bands': bands})
+
+def listing(request):
+    listing = Listing.objects.all()
+    return render(request, 'listings/listings.html',
+                  context={'listings': listing})
